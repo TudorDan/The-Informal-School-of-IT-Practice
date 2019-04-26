76d67a837eefa20b54cc8cae539401768f0d6946
TudorDan <tudor.ist@gmail.com>
2019-04-26 20:02:37 +0300

    Add HTML review exercises

---
+++ Homework9Recap/exRecapitularePart1.js
@@ -0,0 +1,3 @@
+/** Exercitii recapitulare - part 1
+Problema 1.
+Implementati o functie JavaScript care primeste ca si parametru un numar si returneaza true daca numarul este par si fals daca este impar.*/
